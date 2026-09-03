fb484147c98f9997d735faed84d3c276f4bbb5ea
anil115623-svg <anil115623@gmail.com>
2026-09-03 07:17:54 +0530

    Create Factorial.java

---
+++ 01-Daily Practice/Practice-03/Factorial.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+interface Factorial {
+    int calculate(int n);
+}
+
+
+class FactorialCalculator implements Factorial {
+
+    public int calculate(int n) {
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        FactorialCalculator obj = new FactorialCalculator();
+
+        System.out.println("Factorial of " + n + " = " + obj.calculate(n));
+
+        sc.close();
+    }
+}
